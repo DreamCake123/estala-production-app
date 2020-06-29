6b7a9ea812345b58da9abd88526c0d16284996e9
DreamCake123 <dreamcake123456@gmail.com>
2020-06-29 09:03:52 +0800

    Rename fixes

--- routers/submission.js
+++ routers/submission.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const blueprintsData = require('../models/Blueprint');
+const productionData = require('../models/productionData');
 
 router.post('/', async (req, res) => {
-    var bp = new blueprintsData({
+    var bp = new productionData({
         typeName: req.body.typeName,
         typeId: req.body.typeId,
         ownerName: req.body.ownerName,
